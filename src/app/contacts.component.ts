5ebc4456b444cd8484fd44b3fa1a6bdae70e2596
Oskari Okko Ojala <okko@frantic.com>
2016-09-05 12:25:19 +0300

    ex 3

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -2,29 +2,15 @@ import { Component } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
 import { Contact } from './models/contact';
 
+import { CONTACT_DATA } from './data/contact-data';
+
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css']
 })
-export class ContactsAppComponent {
-  private contact: Contact;
 
-  constructor() {
-    this.contact = {
-      id: 7,
-      name: 'Diana Ellis',
-      email: '',
-      phone: '',
-      birthday: '',
-      website: '',
-      image: '/assets/images/7.jpg',
-      address: {
-        street: '6554 park lane',
-        zip: '43378',
-        city: 'Rush',
-        country: 'United States'
-      }
-    };
-  }
+export class ContactsAppComponent {
+  private contacts: Array<Contact> = CONTACT_DATA;
+
 }
