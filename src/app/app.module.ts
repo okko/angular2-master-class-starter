53cb9748654309378d5b7d279f7b9792bbf13dfa
Oskari Okko Ojala <okko@frantic.com>
2016-09-05 12:50:04 +0300

    ex 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
 import { Contact } from './models/contact';
+import { ContactsService } from './contacts.service';
 
 @NgModule({
   declarations: [ContactsAppComponent, ContactsHeaderComponent],
+  providers: [ContactsService],
   imports: [BrowserModule],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
-
 }
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
 import { Contact } from './models/contact';
-
-import { CONTACT_DATA } from './data/contact-data';
+import { ContactsService } from './contacts.service';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
-  styleUrls: ['contacts.component.css']
+  styleUrls: ['contacts.component.css'],
+  providers: [ContactsService]
 })
 
-export class ContactsAppComponent {
-  private contacts: Array<Contact> = CONTACT_DATA;
+export class ContactsAppComponent implements OnInit {
+  contacts: Array<Contact>;
+
+  constructor(private contactsService: ContactsService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactsService.getContacts();
+  }
 
 }
